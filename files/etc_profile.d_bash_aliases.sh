a16f5b76a27183f2e266147b0fd2bdeebdcc41f1
Drew Nall <devnall@gmail.com>
2019-10-21 16:51:17 -0400

    Shell aliases, ntp config, raspi-blacklist

---
+++ files/etc_profile.d_bash_aliases.sh
@@ -0,0 +1,27 @@
+# Super user
+alias _='sudo'
+
+# Directory listing
+alias l='ls -lAhF --color=auto"
+alias ll='ls -lhF --color=auto"
+alias la='ls -AhF --color=auto"
+alias lsa='ls -lahF --color=auto"
+alias lsal='ls -lahF --color=auto | less -R"
+
+# Directory navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias pu="pushd"
+alias po="popd"
+
+# Other
+alias rm='rm -i'
+alias du='du -h'
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias top="htop"
+alias grep='grep --color=auto'
+alias ports='netstat -tulanp'
